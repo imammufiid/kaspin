8e7b04b6a0046b4d9f6a66ef34f434a75d7821d0
imufiid <imufiid98@gmail.com>
2021-10-14 11:10:35 +0700

    init view binding and add navigation

--- app/src/main/java/com/example/kasirpintartest/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/kasirpintartest/ui/main/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.kasirpintartest.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.kasirpintartest.R
+import com.example.kasirpintartest.databinding.ActivityMainBinding
+import com.example.kasirpintartest.ui.addupdate.AddUpdateActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var _bind: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _bind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_bind.root)
+
+        _bind.btnAdd.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.btn_add -> startActivity(Intent(this, AddUpdateActivity::class.java))
+        }
     }
 }
