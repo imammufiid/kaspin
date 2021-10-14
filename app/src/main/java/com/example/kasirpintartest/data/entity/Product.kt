16a04ac51469719b1a2b9d0dd0dedb55e3686957
imufiid <imufiid98@gmail.com>
2021-10-14 09:24:50 +0700

    create entity data class

---
+++ app/src/main/java/com/example/kasirpintartest/data/entity/Product.kt
@@ -0,0 +1,12 @@
+package com.example.kasirpintartest.data.entity
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Product (
+    val id: Int,
+    val code: String,
+    val name: String,
+    val stock: Int
+): Parcelable
