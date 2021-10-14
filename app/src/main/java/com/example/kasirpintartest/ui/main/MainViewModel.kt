2ff81a09ec9ad4c3b7f8a1b7f227413316760e35
imufiid <imufiid98@gmail.com>
2021-10-14 17:25:11 +0700

    create main view model

---
+++ app/src/main/java/com/example/kasirpintartest/ui/main/MainViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.kasirpintartest.ui.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.kasirpintartest.data.entity.Product
+import com.example.kasirpintartest.data.RepositoryImpl
+import kotlinx.coroutines.launch
+
+class MainViewModel(private val repo: RepositoryImpl) : ViewModel() {
+    private var _loading = MutableLiveData<Boolean>()
+    val loading: LiveData<Boolean> = _loading
+
+    private var _products = MutableLiveData<List<Product>>()
+    val products: LiveData<List<Product>> = _products
+
+    fun getProducts() {
+        viewModelScope.launch {
+            _loading.postValue(true)
+            val result = repo.getProducts()
+            _products.postValue(result.value)
+            _loading.postValue(false)
+        }
+    }
+}
