b39f4770aff8e0c314d3628479c86f0dffc6f7ac
imufiid <imufiid98@gmail.com>
2021-10-14 11:15:32 +0700

    handle button submit for update and add data

--- app/src/main/java/com/example/kasirpintartest/ui/addupdate/AddUpdateActivity.kt
+++ app/src/main/java/com/example/kasirpintartest/ui/addupdate/AddUpdateActivity.kt
@@ -1,13 +1,22 @@
 package com.example.kasirpintartest.ui.addupdate
 
+import android.content.ContentValues
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.kasirpintartest.R
+import com.example.kasirpintartest.data.db.DatabaseContract
+import com.example.kasirpintartest.data.db.DatabaseContract.ProductColumns.Companion.CODE
+import com.example.kasirpintartest.data.db.DatabaseContract.ProductColumns.Companion.DATE
 import com.example.kasirpintartest.data.db.ProductHelper
 import com.example.kasirpintartest.data.entity.Product
 import com.example.kasirpintartest.databinding.ActivityAddUpdateBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
-class AddUpdateActivity : AppCompatActivity() {
+class AddUpdateActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var _bind: ActivityAddUpdateBinding
 
     private var isEdit = false
@@ -36,6 +45,8 @@ class AddUpdateActivity : AppCompatActivity() {
         product = intent.getParcelableExtra(EXTRA_PRODUCT)
         isEditProduct()
         setupActionBar()
+
+        _bind.btnSubmit.setOnClickListener(this)
     }
 
     private fun setupActionBar() {
@@ -67,5 +78,55 @@ class AddUpdateActivity : AppCompatActivity() {
         } else {
             product = Product()
         }
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.btn_submit) {
+            val name = _bind.edtName.text.toString().trim()
+            val stock = _bind.edtStock.text.toString().trim()
+            if (title.isEmpty()) {
+                _bind.edtName.error = "Field can not be blank"
+                return
+            }
+            product?.name = name
+            product?.stock = stock.toInt()
+            val intent = Intent().apply {
+                putExtra(EXTRA_PRODUCT, product)
+                putExtra(EXTRA_POSITION, position)
+            }
+
+            val values = ContentValues().apply {
+                put(DatabaseContract.ProductColumns.NAME, name)
+                put(DatabaseContract.ProductColumns.STOCK, stock)
+            }
+
+            if (isEdit) {
+                val result = productHelper.update(product?.id.toString(), values).toLong()
+                if (result > 0) {
+                    setResult(RESULT_UPDATE, intent)
+                    finish()
+                } else {
+                    Toast.makeText(this, "Gagal mengupdate data", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                product?.date = getCurrentDate()
+                values.put(DATE, getCurrentDate())
+                values.put(CODE, "BRG_${System.currentTimeMillis()}")
+                val result = productHelper.insert(values)
+                if (result > 0) {
+                    product?.id = result.toInt()
+                    setResult(RESULT_ADD, intent)
+                    finish()
+                } else {
+                    Toast.makeText(this, "Gagal menambah data", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun getCurrentDate(): String {
+        val dateFormat = SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale.getDefault())
+        val date = Date()
+        return dateFormat.format(date)
     }
 }
