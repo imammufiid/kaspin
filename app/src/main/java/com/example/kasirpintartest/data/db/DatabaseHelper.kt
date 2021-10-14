f3610d1b93d0e04e006a3f7d57c6c1ad67f3ba21
imufiid <imufiid98@gmail.com>
2021-10-14 09:37:50 +0700

    implement sqlite open helper for exec sql

--- app/src/main/java/com/example/kasirpintartest/data/db/DatabaseHelper.kt
+++ app/src/main/java/com/example/kasirpintartest/data/db/DatabaseHelper.kt
@@ -1,5 +1,8 @@
 package com.example.kasirpintartest.data.db
 
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
 import com.example.kasirpintartest.data.db.DatabaseContract.ProductColumns.Companion.CODE
 import com.example.kasirpintartest.data.db.DatabaseContract.ProductColumns.Companion.DATE
 import com.example.kasirpintartest.data.db.DatabaseContract.ProductColumns.Companion.NAME
@@ -7,7 +10,7 @@ import com.example.kasirpintartest.data.db.DatabaseContract.ProductColumns.Compa
 import com.example.kasirpintartest.data.db.DatabaseContract.ProductColumns.Companion.TABLE_NAME
 import com.example.kasirpintartest.data.db.DatabaseContract.ProductColumns.Companion._ID
 
-internal class DatabaseHelper {
+internal class DatabaseHelper(context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     companion object {
         private const val DATABASE_NAME = "dbkasirpntar"
         private const val DATABASE_VERSION = 1
@@ -17,5 +20,14 @@ internal class DatabaseHelper {
                 " $NAME VARCHAR(255)" +
                 " $STOCK INTEGER NOT NULL" +
                 " $DATE TEXT NOT NULL),"
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL(SQL_CREATE_TABLE_PRODUCT)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
+        onCreate(db)
     }
 }
