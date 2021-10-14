a20fd13ae520155f9aa07e97571bbacce43ad91f
imufiid <imufiid98@gmail.com>
2021-10-14 17:23:46 +0700

    create mapping data helper

---
+++ app/src/main/java/com/example/kasirpintartest/helper/MappingHelper.kt
@@ -0,0 +1,26 @@
+package com.example.kasirpintartest.helper
+
+import android.database.Cursor
+import com.example.kasirpintartest.data.db.DatabaseContract.ProductColumns.Companion.CODE
+import com.example.kasirpintartest.data.db.DatabaseContract.ProductColumns.Companion.DATE
+import com.example.kasirpintartest.data.db.DatabaseContract.ProductColumns.Companion.NAME
+import com.example.kasirpintartest.data.db.DatabaseContract.ProductColumns.Companion.STOCK
+import com.example.kasirpintartest.data.db.DatabaseContract.ProductColumns.Companion._ID
+import com.example.kasirpintartest.data.entity.Product
+
+object MappingHelper {
+    fun mapCursorToArrayList(productCursor: Cursor?): ArrayList<Product> {
+        val productList = ArrayList<Product>()
+        productCursor?.apply {
+            while (moveToNext()) {
+                val id = getInt(getColumnIndexOrThrow(_ID))
+                val name = getString(getColumnIndexOrThrow(NAME))
+                val code = getString(getColumnIndexOrThrow(CODE))
+                val stock = getString(getColumnIndexOrThrow(STOCK)).toInt()
+                val date = getString(getColumnIndexOrThrow(DATE))
+                productList.add(Product(id, code, name, stock, date))
+            }
+        }
+        return productList
+    }
+}
