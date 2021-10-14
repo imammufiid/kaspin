5d29570a4940022bdf0653d60d04289cd4f6cc85
imufiid <imufiid98@gmail.com>
2021-10-14 10:16:41 +0700

    create new activity addupdate

---
+++ app/src/main/java/com/example/kasirpintartest/ui/addupdate/AddUpdateActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kasirpintartest.ui.addupdate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kasirpintartest.R
+
+class AddUpdateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_update)
+    }
+}
