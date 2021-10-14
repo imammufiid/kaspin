2adf42cfc3dc3d611286a15ef3e8799875a56a5c
imufiid <imufiid98@gmail.com>
2021-10-14 10:27:05 +0700

    update config activity

--- app/src/main/java/com/example/kasirpintartest/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/kasirpintartest/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.kasirpintartest
+package com.example.kasirpintartest.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kasirpintartest.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
