4f78149da5acfdf989e9226e69fa78b66fc8b86d
imufiid <imufiid98@gmail.com>
2021-10-15 12:11:45 +0700

    create TransactionActivity.kt

---
+++ app/src/main/java/com/example/kasirpintartest/ui/transaction/TransactionActivity.kt
@@ -0,0 +1,126 @@
+package com.example.kasirpintartest.ui.transaction
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.kasirpintartest.R
+import com.example.kasirpintartest.data.entity.ProductCheckout
+import com.example.kasirpintartest.databinding.ActivityTransactionBinding
+import com.example.kasirpintartest.ui.checkout.CheckoutActivity
+import com.example.kasirpintartest.viewmodel.ViewModelFactory
+
+class TransactionActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var _bind: ActivityTransactionBinding
+    private lateinit var transactionAdapter: TransactionAdapter
+    private var productCheckout: List<ProductCheckout> = listOf()
+
+    private val viewModel by lazy {
+        ViewModelProvider(
+            this,
+            ViewModelFactory.getInstance(this)
+        )[TransactionViewModel::class.java]
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _bind = ActivityTransactionBinding.inflate(layoutInflater)
+        setContentView(_bind.root)
+
+        supportActionBar?.title = getString(R.string.transaction)
+        _bind.btnCheckout.setOnClickListener(this)
+
+        viewModel.getProducts()
+        initRV()
+        observeVM()
+    }
+
+    private fun observeVM() {
+        viewModel.loading.observe(this, {
+            if (it) {
+                _bind.progressbar.visibility = View.VISIBLE
+            } else {
+                _bind.progressbar.visibility = View.GONE
+            }
+        })
+        viewModel.products.observe(this, { product ->
+            transactionAdapter.addData(product.filter { it.stock!! > 0 })
+        })
+        viewModel.checkout.observe(this, { product ->
+            productCheckout = product
+            if (product.isNotEmpty()) {
+                _bind.btnCheckout.text = getString(R.string.checkout, product?.size)
+                _bind.btnCheckout.visibility = View.VISIBLE
+            } else {
+                _bind.btnCheckout.visibility = View.GONE
+            }
+        })
+    }
+
+
+    private fun initRV() {
+        transactionAdapter = TransactionAdapter { product ->
+            viewModel.setProductCheckout(product)
+        }
+
+        _bind.rvProducts.apply {
+            layoutManager = LinearLayoutManager(this@TransactionActivity)
+            setHasFixedSize(true)
+            adapter = transactionAdapter
+        }
+    }
+
+    override fun onBackPressed() {
+        val dialogMessage = getString(R.string.message_delete_product)
+        val dialogTitle = getString(R.string.title_cancle_transaction)
+
+        if (productCheckout.isNotEmpty()) {
+            val alertDialogBuilder = AlertDialog.Builder(this)
+            alertDialogBuilder.setTitle(dialogTitle)
+            alertDialogBuilder
+                .setMessage(dialogMessage)
+                .setCancelable(false)
+                .setNegativeButton(getString(R.string.no)) { dialog, _ -> dialog.cancel() }
+                .setPositiveButton(getString(R.string.yes)) { _, _ ->
+                    finish()
+                }
+            val alertDialog = alertDialogBuilder.create()
+            alertDialog.show()
+        } else {
+            finish()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.transaction_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.order_menu -> {
+                return true
+            }
+        }
+        return false
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_checkout -> {
+                startActivity(Intent(this, CheckoutActivity::class.java).apply {
+                    putExtra(
+                        CheckoutActivity.PRODUCTS_EXTRAS,
+                        productCheckout as ArrayList<ProductCheckout>
+                    )
+                })
+                finish()
+            }
+        }
+    }
+}
