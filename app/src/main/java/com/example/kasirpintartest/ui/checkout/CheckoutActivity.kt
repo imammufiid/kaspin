9ffee32b533d631207df3ad3601db874916c3e58
imufiid <imufiid98@gmail.com>
2021-10-15 15:33:08 +0700

    handle product checkout from firebase

--- app/src/main/java/com/example/kasirpintartest/ui/checkout/CheckoutActivity.kt
+++ app/src/main/java/com/example/kasirpintartest/ui/checkout/CheckoutActivity.kt
@@ -10,25 +10,29 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kasirpintartest.R
+import com.example.kasirpintartest.data.entity.Order
 import com.example.kasirpintartest.data.entity.ProductCheckout
 import com.example.kasirpintartest.databinding.ActivityCheckoutBinding
 import com.example.kasirpintartest.ui.transactionsuccess.TransactionSuccessActivity
 import com.example.kasirpintartest.viewmodel.ViewModelFactory
 import com.google.android.material.snackbar.Snackbar
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import java.text.SimpleDateFormat
 import java.util.*
 
 class CheckoutActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var _bind: ActivityCheckoutBinding
-    private var products: List<ProductCheckout>? = null
+    private var products: MutableList<ProductCheckout>? = null
     private lateinit var checkoutAdapter: CheckoutAdapter
     private lateinit var progressDialog: ProgressDialog
     private lateinit var dbRef: DatabaseReference
+    private var isLocalSource: Boolean? = true
+    private var orderID: String? = ""
 
     companion object {
         const val PRODUCTS_EXTRAS = "products_extras"
+        const val IS_LOCAL = "is_local"
+        const val ORDER_ID = "order_id"
     }
 
     private val viewModel by lazy {
@@ -46,6 +50,9 @@ class CheckoutActivity : AppCompatActivity(), View.OnClickListener {
         _bind.btnSave.setOnClickListener(this)
 
         products = intent.getParcelableArrayListExtra(PRODUCTS_EXTRAS)
+        isLocalSource = intent.getBooleanExtra(IS_LOCAL, true)
+        orderID = intent.getStringExtra(ORDER_ID)
+
         dbRef = FirebaseDatabase.getInstance().getReference("Orders")
         progressDialog = ProgressDialog(this)
         initRV()
@@ -60,10 +67,31 @@ class CheckoutActivity : AppCompatActivity(), View.OnClickListener {
                 checkoutAdapter.updateItem(position, product)
             } else {
                 checkoutAdapter.removeItem(position)
+                products?.removeAt(position)
             }
         }
 
-        checkoutAdapter.addData(products)
+        if (isLocalSource == true) {
+            checkoutAdapter.addData(products)
+        } else {
+            dbRef.child(orderID.toString())
+                .addValueEventListener(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        if (snapshot.exists()) {
+                            val productItem = snapshot.getValue(Order::class.java)
+                            if (productItem != null) {
+                                checkoutAdapter.addData(productItem.product)
+                                products = productItem.product as MutableList<ProductCheckout>
+                            }
+
+                        }
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        showSnackbarMessage(error.message)
+                    }
+                })
+        }
 
         _bind.rvProducts.apply {
             layoutManager = LinearLayoutManager(this@CheckoutActivity)
@@ -82,7 +110,8 @@ class CheckoutActivity : AppCompatActivity(), View.OnClickListener {
         })
         viewModel.updated.observe(this, { result ->
             if (result <= 0) {
-                Toast.makeText(this, getString(R.string.failed_transaction), Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.failed_transaction), Toast.LENGTH_SHORT)
+                    .show()
                 return@observe
             }
             startActivity(Intent(this, TransactionSuccessActivity::class.java))
@@ -137,21 +166,34 @@ class CheckoutActivity : AppCompatActivity(), View.OnClickListener {
                         }
                     }
                 }
+                finish()
             }
             R.id.btn_save -> {
                 progressDialog.apply {
                     setMessage("Save to database")
                 }.show()
-                val orderId = "ODR_${System.currentTimeMillis()}"
-                dbRef.child(orderId).setValue(products)
-                    .addOnCompleteListener {
-                        progressDialog.dismiss()
-                        showSnackbarMessage("Product saved in to firebase")
-                    }
-                    .addOnFailureListener {
-                        progressDialog.dismiss()
-                        it.message?.let { msg -> showSnackbarMessage(msg) }
-                    }
+
+                if (orderID?.isNotEmpty() == true) {
+                    val order = Order(orderID, products)
+                     dbRef.child(orderID.toString()).setValue(order)
+                         .addOnCompleteListener {
+                             progressDialog.dismiss()
+                             showSnackbarMessage("Order updated in to firebase")
+                         }
+                } else {
+                    val orderId = "ODR_${System.currentTimeMillis()}"
+                    val order = Order(orderId, products)
+                    dbRef.child(orderId).setValue(order)
+                        .addOnCompleteListener {
+                            progressDialog.dismiss()
+                            showSnackbarMessage("Order saved in to firebase")
+                        }
+                        .addOnFailureListener {
+                            progressDialog.dismiss()
+                            it.message?.let { msg -> showSnackbarMessage(msg) }
+                        }
+                }
+
             }
         }
     }
