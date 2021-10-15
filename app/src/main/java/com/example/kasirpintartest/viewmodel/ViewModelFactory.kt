8869a1b197c503a609027ac80ac509e4066b995e
imufiid <imufiid98@gmail.com>
2021-10-15 12:08:18 +0700

    added assign some view model

--- app/src/main/java/com/example/kasirpintartest/viewmodel/ViewModelFactory.kt
+++ app/src/main/java/com/example/kasirpintartest/viewmodel/ViewModelFactory.kt
@@ -5,7 +5,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.kasirpintartest.data.RepositoryImpl
 import com.example.kasirpintartest.helper.Injection
+import com.example.kasirpintartest.ui.addupdate.AddUpdateActivity
+import com.example.kasirpintartest.ui.addupdate.AddUpdateViewModel
+import com.example.kasirpintartest.ui.checkout.CheckoutViewModel
 import com.example.kasirpintartest.ui.main.MainViewModel
+import com.example.kasirpintartest.ui.transaction.TransactionViewModel
 
 class ViewModelFactory private constructor(private val repo: RepositoryImpl) :
     ViewModelProvider.NewInstanceFactory() {
@@ -21,6 +25,9 @@ class ViewModelFactory private constructor(private val repo: RepositoryImpl) :
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return when {
             modelClass.isAssignableFrom(MainViewModel::class.java) -> MainViewModel(repo) as T
+            modelClass.isAssignableFrom(TransactionViewModel::class.java) -> TransactionViewModel(repo) as T
+            modelClass.isAssignableFrom(CheckoutViewModel::class.java) -> CheckoutViewModel(repo) as T
+            modelClass.isAssignableFrom(AddUpdateViewModel::class.java) -> AddUpdateViewModel(repo) as T
             else -> throw Throwable("Unknown ViewModel Class: ${modelClass.name}")
         }
     }
