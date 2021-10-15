de0a06406e1199ba0b308b19a51dcc6403a13400
imufiid <imufiid98@gmail.com>
2021-10-15 12:20:23 +0700

    create CheckoutViewModel.kt

---
+++ app/src/main/java/com/example/kasirpintartest/ui/checkout/CheckoutViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.kasirpintartest.ui.checkout
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.kasirpintartest.data.RepositoryImpl
+import com.example.kasirpintartest.data.entity.Product
+import kotlinx.coroutines.launch
+
+class CheckoutViewModel(private val repo: RepositoryImpl): ViewModel() {
+    private var _loading = MutableLiveData<Boolean>()
+    val loading: LiveData<Boolean> = _loading
+
+    private var _updated = MutableLiveData<Int>()
+    val updated: LiveData<Int> = _updated
+
+    fun updateStock(product: Product, qty: Int) {
+        viewModelScope.launch {
+            _loading.postValue(true)
+            val result = repo.updateStock(product, qty)
+            _updated.postValue(result.value)
+            _loading.postValue(false)
+        }
+    }
+}
