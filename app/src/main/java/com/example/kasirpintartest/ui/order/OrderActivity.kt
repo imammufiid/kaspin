16aa8517bcbc81bc3bf5b9155c2953c824dfa046
imufiid <imufiid98@gmail.com>
2021-10-15 12:12:41 +0700

    init Order

---
+++ app/src/main/java/com/example/kasirpintartest/ui/order/OrderActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kasirpintartest.ui.order
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kasirpintartest.R
+
+class OrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order)
+    }
+}
