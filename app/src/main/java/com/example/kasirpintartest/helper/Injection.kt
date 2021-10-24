5f2930e57d91af364f03fd3ab12ff5515eb516b0
imufiid <imufiid98@gmail.com>
2021-10-24 15:22:16 +0700

    Added dependency RemoteDataSource.kt

--- app/src/main/java/com/example/kasirpintartest/helper/Injection.kt
+++ app/src/main/java/com/example/kasirpintartest/helper/Injection.kt
@@ -2,6 +2,7 @@ package com.example.kasirpintartest.helper
 
 import android.content.Context
 import com.example.kasirpintartest.data.LocalDataSource
+import com.example.kasirpintartest.data.remote.RemoteDataSource
 import com.example.kasirpintartest.data.db.ProductHelper
 import com.example.kasirpintartest.data.RepositoryImpl
 
@@ -9,7 +10,8 @@ object Injection {
     fun provideRepository(context: Context): RepositoryImpl {
         val productHelper = ProductHelper.getInstance(context)
         val localDataSource = LocalDataSource.getInstance(productHelper)
+        val remoteDataSource = RemoteDataSource.getInstance()
 
-        return RepositoryImpl.getInstance(localDataSource)
+        return RepositoryImpl.getInstance(localDataSource, remoteDataSource)
     }
 }
